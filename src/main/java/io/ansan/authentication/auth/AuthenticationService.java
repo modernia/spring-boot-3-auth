8571b88054159d135bb071743fc7d181db40ebd1
Miguel Martinez <miguel.emezero@gmail.com>
2023-03-27 19:41:06 -0600

    👾 feat: service for authentication

---
+++ src/main/java/io/ansan/authentication/auth/AuthenticationService.java
@@ -0,0 +1,51 @@
+package io.ansan.authentication.auth;
+
+import io.ansan.authentication.config.JwtService;
+import io.ansan.authentication.user.Role;
+import io.ansan.authentication.user.User;
+import io.ansan.authentication.user.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+  private final UserRepo repository;
+  private final PasswordEncoder passwordEncoder;
+  private final JwtService jwtService;
+  private final AuthenticationManager authenticationManager;
+
+
+  public AuthenticationResponse register(RegisterRequest request) {
+    var user = User.builder()
+        .firstname(request.getFirstname())
+        .lastname(request.getLastname())
+        .email(request.getEmail())
+        .password(passwordEncoder.encode(request.getPassword()))
+        .role(Role.USER)
+        .build();
+    repository.save(user);
+    var jwtToken = jwtService.generateToken(user);
+    return AuthenticationResponse.builder()
+        .token(jwtToken)
+        .build();
+  }
+
+  public AuthenticationResponse authenticate(AuthenticationRequest request) {
+    authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(
+            request.getEmail(),
+            request.getPassword()
+        )
+    );
+
+    var user = repository.findByEmail(request.getEmail()).orElseThrow();
+    var jwtToken = jwtService.generateToken(user);
+    return AuthenticationResponse.builder()
+        .token(jwtToken)
+        .build();
+  }
+}
