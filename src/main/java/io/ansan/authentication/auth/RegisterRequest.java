fbc684a5d1c008e4c35d1e65d5da928334ee3e3a
Miguel Martinez <miguel.emezero@gmail.com>
2023-03-28 15:29:28 -0600

    builder for register request

---
+++ src/main/java/io/ansan/authentication/auth/RegisterRequest.java
@@ -0,0 +1,18 @@
+package io.ansan.authentication.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class RegisterRequest {
+  private String firstname;
+  private String lastname;
+  private String email;
+  private String password;
+
+}
