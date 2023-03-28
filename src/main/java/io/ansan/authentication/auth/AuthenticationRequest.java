774930ea54a86de7d9a967f4a11be31a5a4de02a
Miguel Martinez <miguel.emezero@gmail.com>
2023-03-27 19:38:35 -0600

    👾 feat: request model

---
+++ src/main/java/io/ansan/authentication/auth/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package io.ansan.authentication.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class AuthenticationRequest {
+  private String email;
+  private String password;
+}
